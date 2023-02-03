d7c70440b73f809b543f156e25dad5b1032fdbf6
Roshan5401 <68680320+Roshan5401@users.noreply.github.com>
2023-02-03 17:00:21 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+import models.Customer;
+import models.Product;
+
+public class App {
+    public static void main(String[] args) {
+        HashMap<String,Customer> hm=new HashMap<>();
+        Product p1=new Product("Shaving gel","Gillete",50,20);
+        Product p2=new Product("Soap","Lux",35,15);
+        Product p3=new Product("Shirt","Raymonds",5000,5);
+        Product p4=new Product("Shoe","Nike",2000,25);
+        Product p5=new Product("Bag","Safari",2500,12);
+        
+       
+       while(true)
+       {
+
+       }
+       
+       
+        String name;
+        String email;
+        System.out.println("Enter the name");
+        System.out.println("Enter the email address");
+
+
+    }
+
+}
